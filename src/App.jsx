c59d9c575d9ef0f5b739481a03ddb7adbac94c36
Zaeem Ahmad <60064010+zaeem9236@users.noreply.github.com>
2023-09-25 23:40:03 +0500

    react project created and tailwind installed

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className={`h-screen w-scrren flex flex-col justify-center items-center bg-gray-300`}>
+      <h6 className="text-3xl text-gray-500">react-post</h6>
+    </div>
+  )
+}
+
+export default App
