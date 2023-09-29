b33a456df4cea7bd344066ab63d9bf86e486abd9
Zaeem Ahmad <60064010+zaeem9236@users.noreply.github.com>
2023-09-30 03:06:15 +0500

    Header and Footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-
+import { useEffect, useState } from 'react'
+import { Header, Footer } from './components/index'
+import { useDispatch } from 'react-redux'
+import { authService } from './appwrite/auth'
 function App() {
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
+  }, [])
 
   return (
-    <div className={`h-screen w-scrren flex flex-col justify-center items-center bg-gray-300`}>
+    <div className={`h-screen w-screen bg-gray-200 dark:bg-gray-900`}>
+      <Header />
       <h6 className="text-3xl text-gray-500">react-post</h6>
+      <Footer />
     </div>
   )
 }
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,12 @@
 
 export const Footer = () => {
     return (
-        <div>Footer</div>
+
+        <footer className=" bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+            <div className=" fixed bottom-0 w-full p-4 flex items-center justify-center">
+                    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© {new Date().getFullYear()} <a className="hover:underline">React-post</a>. All Rights Reserved.
+                    </span>
+            </div>
+        </footer>
     )
 }
