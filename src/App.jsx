501809a4efaa1f8b374871c726af80ea979b9497
Zaeem Ahmad <60064010+zaeem9236@users.noreply.github.com>
2023-10-01 07:14:31 +0500

    loading logic updated

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,17 @@ function App() {
 
   useEffect(() => {
     authService.getCurrentUser()
-    .then((data) => {
-      console.log(data)
-      setLoading(false)
-    })
-    .catch(err => console.log(err))
+      .then((data) => {
+        console.log(data)
+        setLoading(false)
+      })
+      .catch(err => console.log(err))
   }, [])
 
   return (
     <div className={`h-screen w-screen bg-gray-200 dark:bg-gray-900`}>
       <Header />
-      {!loading? <h6 className="text-3xl text-white">react-post</h6>: <h1 className='text-4xl text-red-600'>Loading</h1>}
+      {loading ? <h1 className='text-4xl text-red-600'>Loading</h1> : <h6 className="text-3xl text-white">react-post</h6>}
       <Footer />
     </div>
   )
