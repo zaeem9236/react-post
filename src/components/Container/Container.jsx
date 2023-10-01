56020defeba3d6e0f3e2cc99f8dad9db8dd17b09
Zaeem Ahmad <60064010+zaeem9236@users.noreply.github.com>
2023-10-01 07:58:28 +0500

    container component added

---
+++ src/components/Container/Container.jsx
@@ -0,0 +1,4 @@
+
+export const Container = () => {
+  return <div>Container component</div>;
+};
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,9 @@
 import { Header } from "./Header/Header";
 import { Footer } from "./Footer/Footer";
+import { Container } from "./Container/Container";
 
 export {
     Header,
-    Footer
+    Footer,
+    Container
 }
