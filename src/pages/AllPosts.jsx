4e413e9c2ee197cd33e819ed6e85f3adfc4ce91e
Zaeem Ahmad <60064010+zaeem9236@users.noreply.github.com>
2024-01-24 22:42:34 +0500

    Loader is added in All-Posts page

--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -1,29 +1,29 @@
 import React, { useState, useEffect } from 'react'
 import { Container, PostCard } from '../components'
 import { dbService } from "../appwrite/db";
+import { Spinner } from '../components/index'
 
 function AllPosts() {
   const [posts, setPosts] = useState([])
+  const [loader, setLoader] = useState(true)
   useEffect(() => {
     dbService.getPosts([]).then((posts) => {
       if (posts) {
         setPosts(posts.documents)
       }
     })
-
+    setLoader(false)
   }, [])
 
-  return (
+  return loader ? (<Spinner />) : (
     <div className='w-full py-8'>
-      {/* <Container> */}
-        <div className='flex flex-wrap'>
-          {posts.map((post, index) => (
-            <div key={post.$id} className='p-2 w-1/4'>
-              <PostCard {...post} />
-            </div>
-          ))}
-        </div>
-      {/* </Container> */}
+      <div className='flex flex-wrap'>
+        {posts.map((post, index) => (
+          <div key={post.$id} className='p-2 w-1/4'>
+            <PostCard {...post} />
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
